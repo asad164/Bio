b8ea8ae74a57c8eca1fe23972b1ed15b63fd21e3
asad164 <40576917+asad164@users.noreply.github.com>
2021-06-05 19:15:25 +0600

    Create bio.py

---
+++ bio.py
@@ -0,0 +1,10 @@
+class Mybio:
+  def __init__(self, n,a):
+        self.name = n
+        self.age = a
+  def my_info(self):
+        print(self.name, self.age)
+
+iam = Mybio('asad', 31)
+iam.my_info()
+  
