ce24d90471b37108483a7b7ec27265642fcf9c47
asad164 <a.zaman.law@gmail.com>
2021-06-05 20:02:42 +0600

    added interest

--- bio.py
+++ bio.py
@@ -2,9 +2,10 @@ class Mybio:
   def __init__(self, n,a):
         self.name = n
         self.age = a
-  def my_info(self):
-        print(self.name, self.age)
+  def my_info(self, i):
+      self.interest = i
+      print('Name:', self.name, 'Age: ', self.age, 'and Interest:', self.interest)
 
 iam = Mybio('asad', 31)
-iam.my_info()
+iam.my_info('Development with Django')
   
